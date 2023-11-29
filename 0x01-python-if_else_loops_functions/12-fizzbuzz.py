d8a26ff3b3d9aaa70f3ec148c5d95ae7b739b05f
monic-kim <wambuimonica506@gmail.com>
2023-11-29 03:32:12 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 != 0:
-            print("Fizz" , end='')
+            print("Fizz ", end='')
         elif x % 5 == 0 and x % 3 != 0:
             print("Buzz ", end='')
         elif x % 5 == 0 and x % 3 == 0:
