317e10a962b4965438caa9d542e0159d58a4fbaa
monic-kim <wambuimonica506@gmail.com>
2023-12-03 01:32:39 -0800

    tuple with len of string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if not sentence:
         sen_len = 0
-        first_char = 0
+        first_char = None
     else:
         sen_len = len(sentence)
         first_char = sentence[0]
