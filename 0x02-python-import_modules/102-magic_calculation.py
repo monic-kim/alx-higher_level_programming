f75a3da7a2cb06e8b20cbcd8af072d6719bb552e
monic-kim <wambuimonica506@gmail.com>
2023-12-03 03:51:19 -0800

    task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation(a, b)
+    """Match the bytecode  provided"""
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for x in range(4, 6)
